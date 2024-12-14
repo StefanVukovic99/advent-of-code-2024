0d78cc9e48b163033ffbc8eb21f6afa18f12e7a0
Stefan Vukovic <stefanvukovic44@gmail.com>
2024-12-14 22:10:21 +0100

    day 10 rust

--- src/day10.rs
+++ src/day10.rs
@@ -3,24 +3,13 @@ use aoc_runner_derive::{aoc};
 const MATRIX_SIZE: usize = 50;
 const DIRECTIONS : [(isize, isize); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
 
-fn parse_input(input: &str) -> [[u8; MATRIX_SIZE]; MATRIX_SIZE] {
-    let mut matrix = [[0; MATRIX_SIZE]; MATRIX_SIZE];
-    for (i, line) in input.lines().enumerate() {
-        for (j, c) in line.chars().enumerate() {
-            matrix[i][j] = c.to_digit(10).unwrap() as u8;
-        }
-    }
-    matrix
-
-}
-
 fn get_reachable_nines_recursive(matrix: &[[u8; MATRIX_SIZE]; MATRIX_SIZE], x: usize, y: usize) -> Vec<(usize, usize)> {
     let number = matrix[x][y];
     if(number == 9) {
         return vec![(x, y)];
     }
 
-    let mut empty : Vec<(usize, usize)> = vec![];
+    let empty : Vec<(usize, usize)> = vec![];
     let value =  DIRECTIONS.iter().fold(empty, |mut acc, (dx, dy)| {
         let new_x = x as isize + dx;
         let new_y = y as isize + dy;
@@ -36,8 +25,6 @@ fn get_reachable_nines_recursive(matrix: &[[u8; MATRIX_SIZE]; MATRIX_SIZE], x: u
     });
 
     let value = value.into_iter().collect::<std::collections::HashSet<_>>().into_iter().collect::<Vec<_>>();
-
-    // println!("Value of {} at ({}, {}) is {:?}", number, x, y, value);
 
     value
     
@@ -63,9 +50,47 @@ pub fn part1(input: &str) -> usize {
     })
 }
 
+
+fn get_recursive_field_value(matrix: &[[u8; MATRIX_SIZE]; MATRIX_SIZE], x: usize, y: usize) -> usize {
+    let number = matrix[x][y];
+    if(number == 9) {
+        return 1;
+    }
+
+    let value =  DIRECTIONS.iter().fold(0, |acc, (dx, dy)| {
+        let new_x = x as isize + dx;
+        let new_y = y as isize + dy;
+        if new_x < 0 || new_x >= MATRIX_SIZE as isize || new_y < 0 || new_y >= MATRIX_SIZE as isize {
+            return acc;
+        }
+        let new_x = new_x as usize;
+        let new_y = new_y as usize;
+        if matrix[new_x][new_y] == number + 1 {
+            return acc + get_recursive_field_value(matrix, new_x, new_y);
+        }
+        return acc;
+    });
+
+    value
+    
+}
+
 #[aoc(day10, part2)]
 pub fn part2(input: &str) -> usize {
-    let matrix = parse_input(input);
-    0
+    let mut matrix = [[0; MATRIX_SIZE]; MATRIX_SIZE];
+    let mut zeroes : Vec<(usize, usize)> = Vec::new();
+    for (i, line) in input.lines().enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            matrix[i][j] = c.to_digit(10).unwrap() as u8;
+            if matrix[i][j] == 0 {
+                zeroes.push((i, j));
+                
+            }
+        }
+    }
     
+    // return sum of values of zeroes
+    zeroes.iter().fold(0, |acc, (x, y)| {
+        acc + get_recursive_field_value(&matrix, *x, *y)
+    })
 }
