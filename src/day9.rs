d91161238302fb77883a33cae57f0777ca521fdf
Stefan Vukovic <stefanvukovic44@gmail.com>
2024-12-14 21:42:53 +0100

    day 9 rust

--- src/day9.rs
+++ src/day9.rs
@@ -2,6 +2,63 @@ use aoc_runner_derive::{aoc};
 
 #[aoc(day9, part1)]
 pub fn part1(input: &str) -> usize {
+    let mut sane_blocks = get_sane_blocks(input);
+
+    let mut i = 0;
+    while i < sane_blocks.len() {
+        if sane_blocks[i].0 != 0 {
+            i += 1;
+            continue;
+        }
+
+        // Process block at index i
+        let mut block = sane_blocks[i];
+
+        let mut j = sane_blocks.len() - 1;
+        while j > i {
+            let block2 = sane_blocks[j];
+            if block2.0 == 0 {
+                j -= 1;
+                continue;
+            }
+
+            if block2.1 > block.1 {
+                sane_blocks[i].0 = block2.0;
+                sane_blocks[j].1 -= block.1;
+                break;
+            } else if block2.1 == block.1 {
+                sane_blocks[i].0 = block2.0;
+                sane_blocks.remove(j);
+                break;
+            } else {
+                sane_blocks[i] = (block2.0, block2.1);
+                sane_blocks.insert(i + 1, (0, block.1 - block2.1));
+                sane_blocks.remove(j+1);
+                break;
+            }
+        }
+
+        i += 1;
+    }
+
+
+    let mut checskum = 0;
+    let mut i = 0;
+    for mut block in sane_blocks {
+        if block.0 == 0 {
+            continue;
+        }
+        block.0 -= 1;
+
+        let value =  block.1 * block.0 * i + block.0 * block.1*(block.1-1)/2;
+        checskum += value;
+        i+= block.1;
+    }
+
+    checskum
+}
+
+fn get_sane_blocks(input: &str) -> Vec<(usize, usize)> {
     let mut sane_blocks: Vec<(usize, usize)> = Vec::new();
 
     // Parse the input into sane_blocks
@@ -17,64 +74,61 @@ pub fn part1(input: &str) -> usize {
         }
     }
 
-    println!("Initial sane_blocks: {:?}", sane_blocks);
-
-    let mut i = 0;
-    while i < sane_blocks.len() {
-        if sane_blocks[i].0 != 0 {
-            i += 1;
-            continue;
-        }
-
-        // Process block at index i
-        let mut block = sane_blocks[i];
-        println!("Processing block {:?} at index {}", block, i);
-
-        let mut j = sane_blocks.len() - 1;
-        while j > i {
-            let block2 = sane_blocks[j];
-            if block2.0 == 0 {
-                j -= 1;
-                continue;
-            }
-
-            println!(
-                "Matching block {:?} at index {} with block {:?} at index {}",
-                block, i, block2, j
-            );
-
-            if block2.1 > block.1 {
-                sane_blocks[i].0 = block2.0;
-                sane_blocks[j].1 -= block.1;
-                println!("1 Moved block {:?}({:?}) into {:?}({:?}) ", j, block2, i, block);
-                break;
-            } else if block2.1 == block.1 {
-                sane_blocks[i].0 = block2.0;
-                sane_blocks.remove(j);
-                println!("2 Moved block {:?}({:?}) into {:?}({:?}) ", j, block2, i, block);
-                break;
-            } else {
-                sane_blocks[i] = (block2.0, block2.1);
-                sane_blocks.insert(i + 1, (0, block.1 - block2.1));
-                sane_blocks.remove(j+1);
-                println!("3 Moved block {:?}({:?}) into {:?}({:?}) ", j, block2, i, block);
-                break;
-            }
-        }
-
-        i += 1;
-    }
-
-    println!("Final sane_blocks: {:?}", sane_blocks);
-
-    let sum = sane_blocks.iter()
-        .filter(|x| x.0 != 0)
-        .fold(0, |acc, x| acc + (x.0 - 1 ) * x.1 + x.0 * (x.1*x.1 - x.1) / 2);
-
-    sum
+    sane_blocks
 }
 
 #[aoc(day9, part2)]
 pub fn part2(input: &str) -> usize {
-    0
+    let mut sane_blocks = get_sane_blocks(input);
+
+    let mut j = sane_blocks.len();
+    while j > 0 {
+        j -= 1;
+        if sane_blocks[j].0 == 0 {
+            continue;
+        }
+
+        let block2 = sane_blocks[j];
+
+        let mut i = 0;
+        while i < j {
+            let block = sane_blocks[i];
+            if block.0 != 0 {
+                i += 1;
+                continue;
+            }
+
+            if block2.1 > block.1 {
+                i+=1;
+                continue;
+            } else if block2.1 == block.1 {
+                sane_blocks[i].0 = block2.0;
+                sane_blocks[j].0 = 0;
+                break;
+            } else {
+                sane_blocks[i] = (block2.0, block2.1);
+                sane_blocks.insert(i + 1, (0, block.1 - block2.1));
+                j+=1;
+                sane_blocks[j].0 = 0;
+                break;
+            }
+        }
+    }
+
+
+    let mut checskum = 0;
+    let mut i = 0;
+    for mut block in sane_blocks {
+        if block.0 == 0 {
+            i+= block.1;
+            continue;
+        }
+        block.0 -= 1;
+
+        let value =  block.1 * block.0 * i + block.0 * block.1*(block.1-1)/2;
+        checskum += value;
+        i+= block.1;
+    }
+
+    checskum
 }
