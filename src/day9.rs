890f0b9359ab6723bdcd51646a0c5969355c4e30
Stefan Vukovic <stefanvukovic44@gmail.com>
2024-12-14 18:35:50 +0100

    day 9 wip

--- src/day9.rs
+++ src/day9.rs
@@ -1,67 +1,75 @@
-
 use aoc_runner_derive::{aoc};
 
 #[aoc(day9, part1)]
 pub fn part1(input: &str) -> usize {
     let mut sane_blocks: Vec<(usize, usize)> = Vec::new();
-    // iterate over enumerated chars
+
+    // Parse the input into sane_blocks
     for (i, c) in input.chars().enumerate() {
         let count = c.to_digit(10).unwrap() as usize;
-        if count == 0{
+        if count == 0 {
             continue;
         }
-        if i%2 == 0 {
-            sane_blocks.push((i/2+1, count));
+        if i % 2 == 0 {
+            sane_blocks.push((i / 2 + 1, count));
         } else {
             sane_blocks.push((0, count));
         }
     }
-    
-    println!("{:?}", sane_blocks);
 
-    'outer: for i in 0..sane_blocks.len() {
-        let mut block = sane_blocks[i];
-        if block.0 != 0 {
+    println!("Initial sane_blocks: {:?}", sane_blocks);
+
+    let mut i = 0;
+    while i < sane_blocks.len() {
+        if sane_blocks[i].0 != 0 {
+            i += 1;
             continue;
         }
-        println!("Moving into block {:?}({:?})", i, block);
 
-        for j in (i+1..sane_blocks.len()).rev() {
-            let mut block2 = sane_blocks[j];
+        // Process block at index i
+        let mut block = sane_blocks[i];
+        println!("Processing block {:?} at index {}", block, i);
+
+        let mut j = sane_blocks.len() - 1;
+        while j > i {
+            let block2 = sane_blocks[j];
             if block2.0 == 0 {
+                j -= 1;
                 continue;
             }
-            println!("Moving block {:?}({:?}) into {:?}({:?}) ", j, block2, i, block);
+
+            println!(
+                "Matching block {:?} at index {} with block {:?} at index {}",
+                block, i, block2, j
+            );
+
             if block2.1 > block.1 {
-                block.0 = block2.0;
-                block2.1 -= block.1;
+                sane_blocks[i].0 = block2.0;
+                sane_blocks[j].1 -= block.1;
                 println!("1 Moved block {:?}({:?}) into {:?}({:?}) ", j, block2, i, block);
-                println!("{:?}", sane_blocks);
+                break;
             } else if block2.1 == block.1 {
-                block.0 = block2.0;
+                sane_blocks[i].0 = block2.0;
                 sane_blocks.remove(j);
                 println!("2 Moved block {:?}({:?}) into {:?}({:?}) ", j, block2, i, block);
-                println!("{:?}", sane_blocks);
+                break;
             } else {
-                sane_blocks.splice(
-                    i..i+1, 
-                    [
-                        (block2.0, block2.1),
-                        (block.0, block.1 - block2.1)
-                    ]
-                );
+                sane_blocks[i] = (block2.0, block2.1);
+                sane_blocks.insert(i + 1, (0, block.1 - block2.1));
                 sane_blocks.remove(j+1);
                 println!("3 Moved block {:?}({:?}) into {:?}({:?}) ", j, block2, i, block);
-                println!("{:?}", sane_blocks);
+                break;
             }
-
-            continue 'outer;
         }
+
+        i += 1;
     }
 
-    println!("{:?}", sane_blocks);
-    
-    0
+    println!("Final sane_blocks: {:?}", sane_blocks);
+
+    let sum = sane_blocks.iter().fold(0, |acc, x| acc + (x.0 - 1 ) * x.1 + x.0 * (x.1*x.1 - x.1) / 2);
+
+    sum
 }
 
 #[aoc(day9, part2)]
